0e222f4a6636282ee2ffe9dcfaa76e8443d20e9b
Clément Thion <clement.thion@orange.fr>
2021-03-21 09:05:33 +0100

    initialisation du repo
    création du repository, avec fichier .gitignor, README.md et accueil.php (pour la page d'accueil)

---
+++ accueil.php
@@ -0,0 +1,26 @@
+<!-- Page d'accueil du site (hors connexion)
+Sur cette page on peut :
+    - avoir les infos générales du site
+    - voir les derniers articles récents
+    - se connecter
+-->
+
+<!--==============================================================-->
+<?php require_once("connexion_base.php"); 
+
+?>
+
+<!--==============================================================-->
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title> XXXXXXXXXXXXXX </title>
+    </head>
+
+    <!------------------------------------------------------>
+    <body>
+        <h1> XXXXXXXXXXXXXX </h1>
+        
+    </body>
+</html>
---
+++ building_resssources/tools_array.php
@@ -0,0 +1,12 @@
+
+
+<?php
+/*----------------
+L'objectif de cette fonction est de génèrer un code html afin d'afficher un tableau array php 
+----------------*/
+function echo_array($arr){
+    
+
+}
+
+?>
