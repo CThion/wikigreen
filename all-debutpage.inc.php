42cedbd2a8f9135c2e8d3693954faaf557961e27
Ayress33 <axelplanteyveux@gmail.com>
2021-04-20 16:15:21 +0200

    CSS (en tête)
    Modification du fichier css notrestyle-bootstrap.css + incrémentation d'id au niveau de certaines classes bootstrap

--- all-debutpage.inc.php
+++ all-debutpage.inc.php
@@ -19,8 +19,8 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <img src="images/logo/logo-wikigreen/logo-1/logo.png" alt="" width="350" height="300" /> <a class="navbar-brand" href="#">
-          <span class="navbar-brand mb-0 h1">Pour une meilleure information de l'écologie </span>
+        <img src="images/logo/logo-wikigreen/logo-1/logo300.png" alt="Logo du site" width="250" height="200" /> <a class="navbar-brand" href="#">
+          <span class="navbar-brand mb-0 h1" id = "Citation">Pour une meilleure information de l'écologie </span>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -37,21 +37,25 @@
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Paramétres
                   </a>
+				  
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Luminosité</a></li>
-                    <li><a class="dropdown-item" href="#">Couleur du fond d'écran</a></li>
-                    <li><a class="dropdown-item" href="#">Police d'écriture</a></li>
-                  </ul>
+                    <li>  <a class="dropdown-item" href="#">Luminosité</a>  </li>
+                    <li>  <a class="dropdown-item" href="#">Couleur du fond d'écran</a> </li>
+                    <li>  <a class="dropdown-item" href="#">Police d'écriture</a>  </li>
+                  
+				  
+				  </ul>
+				  
                 </li>
               </div>
             </div>
             <div class="d-flex flex-column bd-highlight mb-3">
               <div class="p-2 bd-highlight"><img src="images/pp/test.jpg" alt="PP" width="150" height="100" /></div>
               <div class="p-2 bd-highlight"><label for="exampleFormControlInput1" id="test2" class="form-label">Veuillez renseigner votre adresse mail : </label>
-                <input type="email" id="test" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <input type="email" id="test2" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
               </div>
-              <div class="p-2 bd-highlight">Mot de passe : </div>
-              <div class="p-2 bd-highlight">Connexion</div>
+              <div class="p-2 bd-highlight"  id="test2" >Mot de passe : </div>
+              <button type="button" class="btn btn-primary" id="Connexion">Connexion</button>
             </div>
           </div>
       </div>
