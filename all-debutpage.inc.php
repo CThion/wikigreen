ddf15e8825686fa07abdac13606083693420d7e8
Ayress33 <63547089+Ayress33@users.noreply.github.com>
2021-05-09 13:48:57 +0200

    Modification de l'en tête
    Réduction de l'en tête il demeure les trois boutons pour permettre la navigation

--- all-debutpage.inc.php
+++ all-debutpage.inc.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
   <!-- Bootstrap CSS : lien avec styles prédéfinis bootstaps-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
@@ -17,76 +18,38 @@
 
 <body>
   <header>
+	
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	
       <div class="container-fluid">
+	  <img src="images/logo/logo-wikigreen/logo-1/logo300.png" alt="Logo du site" width="150" height="100" /> <a class="navbar-brand" href="">
 	  
-        <img src="images/logo/logo-wikigreen/logo-1/logo300.png" alt="Logo du site" width="250" height="200" /> <a class="navbar-brand" href="#">
-		<div class="d-flex flex-row bd-highlight mb-3">
+         
+	 
 		
-			<div class="d-flex flex-column bd-highlight mb-3">	
-			
-			<div class="p-2 bd-highlight">
-			</div>
-			
-			
-		 <div class="align-self-xl-center">
-		 <span class="navbar-brand mb-3 h1" id = "Citation">Pour une meilleure information de l'écologie </span>
-		 </div>
-		 
-		<div class="d-flex flex-row bd-highlight mb-3">	 
-		 <div class="p-2 bd-highlight">
-		 <div class="alert alert-primary" role="alert">
-		 <a href="Qui-sommes-nous.php" class="alert-link">Qui-sommes nous ?</a>
-		 </div>
-		</div>
-		 <div class="p-2 bd-highlight">
-		 <div class="alert alert-primary" role="alert">
-		 <a href="collaborer.php" class="alert-link">Collaborer</a>
-		 </div>
-		 </div>
-		 
-		 <div class="p-2 bd-highlight">
-		 <div class="alert alert-primary" role="alert">
-		 <a href="recherches.php" class="alert-link">Recherches</a>
-		 </div>
-		</div>
-		</div>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	  
+        <li class="nav-item">
+          <a class="nav-link active" id="centre" aria-current="page" href="Qui-sommes-nous.php"> Qui-sommes nous ? </a>
+        </li>
 		
-		</div>
-		  <div class="p-2 bd-highlight">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			 
-            <span class="navbar-toggler-icon"></span>
-			
-          </button>
-		  </div>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="collaborer.php"> Collaborer </a>
+        </li>
 		
-            <div class="p-2 bd-highlight">
-			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-              
-			  </div>
-			  </ul>
-			<div class="d-flex justify-content-end">
-			<div class="p-2 bd-highlight">
-            <div class="d-flex flex-column bd-highlight mb-3">
-              <div class="p-2 bd-highlight"><img src="images/pp/test.jpg" alt="PP" width="150" height="100" /></div>
-              <div class="p-2 bd-highlight"><label for="exampleFormControlInput1" id="test2" class="form-label">Veuillez renseigner votre adresse mail : </label>
-                <input type="email" id="test2" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-              </div>
-              <div class="p-2 bd-highlight"  id="test2" >Mot de passe : </div>
-              <button type="button" class="btn btn-primary" id="Connexion">Connexion</button>
-            </div>
-			</div>
-		  </div>
-		  </div>
-      </div>
+		<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="recherche.php">Recherches</a>
+        </li>
+      
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
 	  </div>
+	  
 	  
     </nav>
   </header>
