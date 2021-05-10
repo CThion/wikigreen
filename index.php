e830a58b047504fdfd1ebaa167622b4e6a62c500
Cobra888 <80754395+Cobra888@users.noreply.github.com>
2021-05-10 10:51:00 +0200

    test push Thomas

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include "all-debutpage.inc.php";
         Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
         Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales.
         Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh.
-        Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+        Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit .
       </div>
       <div class="col-md-4">
         <h1 id="Perso"> Rien que pour vous </h1>
