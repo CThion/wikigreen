816499fb10e002dd99b6cec31445c41803e4ff9a
Clément Thion <clement.thion@orange.fr>
2021-05-10 15:59:57 +0200

    maj disp-art-full => affichage commentaires
    affichage des commentaires en dessous de l'article (disp art full). Requête sql faite en boucle pour avoir le pseudo, pas très propre...

--- disp-art-full.php
+++ disp-art-full.php
@@ -9,9 +9,9 @@ include "all-debutpage.inc.php";
 ==============================================================-->
 
 <?php
-//----récupération des info dans la bdd qui nous intéressent----
-
 $id_art = $_GET["id_art"]; //récupération de l'idée d'article spécifié
+
+//---- SELECT info article ----
 
 //-- table art
 $requete_art = "SELECT * FROM art WHERE id = ?"; //récupération de l'article à l'id donné
@@ -25,7 +25,7 @@ $artroot = $art[0]["artroot"];
 
 //-- table ref
 $id_ref = $art[0]["id_ref"]; //recherche de la clef étrangère dans l'article
-$requete_ref = "SELECT * FROM ref WHERE id = ?"; 
+$requete_ref = "SELECT * FROM ref WHERE id = ?";
 $reponse_ref = $pdo->prepare($requete_ref);
 $reponse_ref->execute(array($id_ref)); //ce qui va dans le "?" de la requête
 $ref = $reponse_ref->fetchAll(); // récupérer tous les enregistrements dans un tableau
@@ -47,56 +47,106 @@ $reponse_sec->execute(array($id_sec));
 $sec = $reponse_sec->fetchAll(); // récupérer tous les enregistrements dans un tableau
 $nbrep_sec = count($art); // connaitre le nombre d'enregistrements
 
-//---- autres requêtes
-
 //-- nbmb_art : nombre de membre ayant contribué sur cet article
-$requete_nbmb_art = "SELECT COUNT(DISTINCT mb.id) FROM mb, art WHERE art.artroot=? AND art.id_mb=mb.id"; 
+$requete_nbmb_art = "SELECT COUNT(DISTINCT mb.id) FROM mb, art WHERE art.artroot=? AND art.id_mb=mb.id";
 $reponse_nbmb_art = $pdo->prepare($requete_nbmb_art);
 $reponse_nbmb_art->execute(array($artroot));
-$nbmb_art = $reponse_nbmb_art->fetchAll(); 
+$nbmb_art = $reponse_nbmb_art->fetchAll();
 
 //-- nbmodif_art : nombre de versions de l'article (= nombre de modifs)
-$requete_nbmodif_art = "SELECT COUNT(*) FROM art WHERE art.artroot=?"; 
+$requete_nbmodif_art = "SELECT COUNT(*) FROM art WHERE art.artroot=?";
 $reponse_nbmodif_art = $pdo->prepare($requete_nbmodif_art);
 $reponse_nbmodif_art->execute(array($artroot));
-$nbmodif_art = $reponse_nbmodif_art->fetchAll(); 
+$nbmodif_art = $reponse_nbmodif_art->fetchAll();
+
+//---- SELECT info commentaires ----
+
+//-- cmt -> #cmt_art //récupération des commentaires sur l'article à l'id donné
+$requete_cmt = "SELECT cmt.id, cmt.texte, cmt.id_mb, cmt.date FROM cmt, cmt_art, art 
+                WHERE cmt.id = cmt_art.id_cmt
+                AND cmt_art.id_art = art.id
+                AND art.id=?";
+$reponse_cmt = $pdo->prepare($requete_cmt);
+$reponse_cmt->execute(array($id_art));
+$cmt = $reponse_cmt->fetchAll();
+$nbrep_cmt = count($cmt);
+
+//-- mb
+
 ?>
+
+
 
 <main role="main">
     <div class="container">
         <div class="col">
-            <!-- titre de section -->
+
+            <!-- affichage de l'article en lui même -->
             <div class="col">
-                <h1 class="titresec">Section : <?php echo $sec[0]["titre"]; ?></h1>
+                <!-- titre de section -->
+                <div class="col">
+                    <h1 class="titresec">Section : <?php echo $sec[0]["titre"]; ?></h1>
+                </div>
+                <!-- titre du thème -->
+                <div class="col">
+                    <h1 class="titrethm">Thème : <?php echo $thm[0]["titre"]; ?></h1>
+                </div>
+                <!-- titre de l'article -->
+                <div class="col">
+                    <h1 class="titreart">Article : <?php echo $art[0]["titre"]; ?></h1>
+                </div>
+                <!-- stat de l'article -->
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item">Dernière modification : <?php echo $art[0]["dateajout"]; ?></li>
+                    <li class="list-group-item">Nombre de modification : <?php echo $nbmodif_art[0][0]; ?></li>
+                    <li class="list-group-item">Nombre de contributeurs : <?php echo $nbmb_art[0][0]; ?></li>
+                </ul>
+                <!-- image de l'article -->
+                <div class="col">
+                    <img src=<?php echo "images/mb-image_upload/image_upload-art/image_art-" . $art[0]['id'] . ".jpg"; ?> alt=<?php echo "image_art-" . $art[0]['id']; ?> width="300" height="300" />
+                </div>
+                <!-- texte de l'article -->
+                <div class="col">
+                    <p class="Texte"><?php echo $art[0]["texte"]; ?></p>
+                </div>
+                <!-- référence de l'article -->
+                <div class="col">
+                    <p><?php echo $ref[0]["titre"]; ?></p>
+                </div>
             </div>
-            <!-- titre du thème -->
+
+            <!-- affichage des commentaires -->
             <div class="col">
-                <h1 class="titrethm">Thème : <?php echo $thm[0]["titre"]; ?></h1>
-            </div>
-            <!-- titre de l'article -->
-            <div class="col">
-                <h1 class="titreart">Article : <?php echo $art[0]["titre"]; ?></h1>
-            </div>
-            <!-- stat de l'article -->
-            <ul class="list-group list-group-horizontal">
-                <li class="list-group-item">Dernière modification : <?php echo $art[0]["dateajout"]; ?></li>
-                <li class="list-group-item">Nombre de modification : <?php echo $nbmodif_art[0][0]; ?></li>
-                <li class="list-group-item">Nombre de contributeurs : <?php echo $nbmb_art[0][0]; ?></li>
-            </ul>
-            <!-- image de l'article -->
-            <div class="col">
-                <img src=<?php echo "images/mb-image_upload/image_upload-art/image_art-".$art[0]['id'].".jpg"; ?> alt=<?php echo "image_art-".$art[0]['id']; ?> width="300" height="300" />
-            </div>
-            <!-- texte de l'article -->
-            <div class="col">
-                <p class="Texte"><?php echo $art[0]["texte"]; ?></p>
-            </div>
-            <!-- référence de l'article -->
-            <div class="col">
-                <p><?php echo $ref[0]["titre"]; ?></p>
+                <h3>Commentaires</h3>
+
+                <!-- https://getbootstrap.com/docs/5.0/components/list-group/ partie custom content -->
+                <div class="list-group">
+                    <?php for ($i = 0; $i < $nbrep_cmt; $i++) { //autant de tout de boucle que de commentaires  
+                        //--table membre, pour récupérer le pseudo de l'auteur du i-ième commentaire
+                        $id_mb = $cmt[$i]['id_mb'];
+                        $requete_mb = "SELECT pseudo FROM mb WHERE id=?"; //récupération toute la table membre
+                        $reponse_mb = $pdo->prepare($requete_mb);
+                        $reponse_mb->execute(array($id_mb));
+                        $mb = $reponse_mb->fetchAll();
+                        $nbrep_mb = count($mb);
+                    ?>
+
+                        <a class="list-group-item list-group-item-action">
+                            <div class="d-flex w-100 justify-content-between">
+                                <!-- le pseudo de l'auteur -->
+                                <h5 class="mb-1"><?php echo $mb[0]['pseudo']; ?></h5>
+                                <!-- la date du commentaire -->
+                                <small><?php echo $cmt[$i]['date']; ?></small>
+                            </div>
+                            <!-- le texte du commentaire -->
+                            <p class="mb-1"><?php echo $cmt[$i]['texte']; ?></p>
+                            <small>And some small print.</small>
+                        </a>
+                    <?php } ?>
+
+                </div>
             </div>
         </div>
-
     </div>
 </main>
 
