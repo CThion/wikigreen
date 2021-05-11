9e03e9d52973a7235c9692c0a110960b123960c3
Clément Thion <clement.thion@orange.fr>
2021-05-11 08:56:39 +0200

    MAJ disp-art-list.php => grosse bourde corrigée
    tous les articles étaient affichés ainsi que toutes leur version en même temps, car n'avait pas été utilisé le champ "artroot" comme filtre.
    PB corrigé => on affiche pour chaque artroot l'article le plus récent

--- disp-art-list.php
+++ disp-art-list.php
@@ -11,12 +11,24 @@ include "all-debutpage.inc.php";
 <?php
 //---- SELECT : récupération des info dans la bdd qui nous intéressent----
 
-//-- table art
-$requete_art = "SELECT * FROM art"; //récupération de tous les articles
-$reponse_art = $pdo->prepare($requete_art);
-$reponse_art->execute(); //ce qui va dans le "?" de la requête
-$art = $reponse_art->fetchAll(); // récupérer tous les enregistrements dans un tableau
-$nbrep_art = count($art); // connaitre le nombre d'enregistrements
+//-- récupération du nombre de artroot distinct (==> nombre d'article affichés)
+$requete_distartroot = "SELECT DISTINCT(art.artroot) FROM art";
+$reponse_distartroot = $pdo->prepare($requete_distartroot);
+$reponse_distartroot->execute();
+$distartroot = $reponse_distartroot->fetchAll();
+$nbrep_distartroot = count($distartroot);
+
+//-- récupération des articles à afficher (on doit pouvoir se passez d'un boucle mais pour l'instant ça ça marche)
+$art = array();
+for ($i = 1; $i <= $nbrep_distartroot; $i++) { //pour chaque valeur de artroot, on prend l'article le plus récent
+    $requete_art = "SELECT * FROM art
+                    WHERE art.artroot = $i
+                    AND art.dateajout IN (SELECT MAX(dateajout) FROM art WHERE art.artroot = $i);";
+    $reponse_art = $pdo->prepare($requete_art);
+    $reponse_art->execute();
+    $arti = $reponse_art->fetchAll();
+    $art[] = $arti[0]; //on ajoute $arti les donnés de l'article en sortie dans $art l'array final 
+}
 ?>
 
 <main role="main">
@@ -28,19 +40,19 @@ $nbrep_art = count($art); // connaitre le nombre d'enregistrements
             <div class="col-6">
                 <div class="list-group" id="list-tab" role="tablist">
                     <?php
-                    for ($i = 0; $i < count($art); $i++) { //autant de ligne qu'il y a d'article
+                    for ($i = 0; $i < $nbrep_distartroot; $i++) { //autant de ligne qu'il y a d'article 
                     ?>
                         <a class="list-group-item list-group-item-action" id=<?php echo "list-" . $i . "-list"; ?> data-bs-toggle="list" href=<?php echo "#list-" . $i; ?> role="tab" aria-controls=<?php echo $i; ?>>
                             <div class="row">
                                 <!-- affichage condensé d'un article en bande -->
                                 <ul class="list-group list-group-horizontal">
                                     <!-- image du thème -->
-                                    <li class="list-group-item"> <img src=<?php echo "images/image_thm/image_thm-".$art[$i]['id_thm'].".jpg"; ?> alt=<?php echo "image_thm-".$art[$i]['id_thm']; ?> width="50" height="50" /></li>
+                                    <li class="list-group-item"> <img src=<?php echo "images/image_thm/image_thm-" . $art[$i]['id_thm'] . ".jpg"; ?> alt=<?php echo "image_thm-" . $art[$i]['id_thm']; ?> width="50" height="50" /></li>
                                     <!-- titre -->
                                     <li class="list-group-item">Titre : <?php echo $art[$i]["titre"]; ?></li>
                                 </ul>
                                 <!-- bouton dirigeant vers article pleine écran -->
-                                
+
                             </div>
                         </a>
                     <?php
@@ -53,15 +65,17 @@ $nbrep_art = count($art); // connaitre le nombre d'enregistrements
             <div class="col-6">
                 <div class="tab-content" id="nav-tabContent">
                     <?php
-                    for ($i = 0; $i < count($art)+1; $i++) {
+                    for ($i = 0; $i < count($art) + 1; $i++) {
                     ?>
                         <div class="tab-pane fade show" id=<?php echo "list-" . $i; ?> role="tabpanel" aria-labelledby=<?php echo "list-" . $i . "-list"; ?>>
                             <!-- titre art -->
                             <h1 class="titreart"><?php echo $art[$i]["titre"]; ?></h1>
+                            <!-- date -->
+                            <p>Version du <?php echo $art[$i]['dateajout'] ?></p>
                             <!-- bouton dirigeant vers l'article en grand  -->
-                            <a class="btn btn-primary" href=<?php echo "disp-art-full.php?id_art=".$art[$i]["id"]; ?> role="button">Link</a>
+                            <a class="btn btn-primary" href=<?php echo "disp-art-full.php?id_art=" . $art[$i]["id"]; ?> role="button">Link</a>
                             <!-- image art -->
-                            <img src=<?php echo "images/mb-image_upload/image_upload-art/image_art-".$art[$i]["id"].".jpg"; ?> alt=<?php echo "image_article-".$art[$i]["id"]; ?> width="150" height="200" />
+                            <img src=<?php echo "images/mb-image_upload/image_upload-art/image_art-" . $art[$i]["id"] . ".jpg"; ?> alt=<?php echo "image_article-" . $art[$i]["id"]; ?> width="150" height="200" />
                             <!-- texte art -->
                             <p class="Texte"> <?php echo $art[$i]["texte"]; ?> </p>
                         </div>
